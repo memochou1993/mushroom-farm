651944904c6a8a2a385c6b19fe3a4aa0d6debf3b
Memo Chou <memochou1993@gmail.com>
2022-06-13 02:35:47 +0800

    Rename variables

--- contracts/Mushroom.sol
+++ contracts/Mushroom.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 contract Mushroom {
     bool public initialized = false;
     uint256 public period = 86400 * 10;
-    uint256 public marketMushroom;
+    uint256 public marketMushrooms;
     address public owner;
-    mapping (address => uint256) public miners;
+    mapping (address => uint256) public farmers;
     mapping (address => uint256) public claimedMushrooms;
     mapping (address => uint256) public lastHarvest;
     mapping (address => address) public referrals;
@@ -17,14 +17,14 @@ contract Mushroom {
 
     function initialize() public {
         require(msg.sender == owner);
-        require(marketMushroom == 0);
+        require(marketMushrooms == 0);
         initialized = true;
-        marketMushroom = 86400 * 1000000;
+        marketMushrooms = 86400 * 1000000;
     }
 
     function buyMushrooms(address _ref) public payable {
         require(initialized);
-        uint256 value = calculate(msg.value, address(this).balance - msg.value, marketMushroom);
+        uint256 value = calculate(msg.value, address(this).balance - msg.value, marketMushrooms);
         value -= getDevFee(value);
         uint256 fee = getDevFee(msg.value);
         payable(owner).transfer(fee);
@@ -34,28 +34,28 @@ contract Mushroom {
 
     function splitMushrooms(address _ref) public {
         require(initialized);
-        if (_ref == msg.sender || _ref == address(0) || miners[_ref] == 0) {
+        if (_ref == msg.sender || _ref == address(0) || farmers[_ref] == 0) {
             _ref = owner;
         }
         if (referrals[msg.sender] == address(0)) {
             referrals[msg.sender] = _ref;
         }
         uint256 used = getMyMushrooms();
-        miners[msg.sender] += used / period;
+        farmers[msg.sender] += used / period;
         claimedMushrooms[msg.sender] = 0;
         lastHarvest[msg.sender] = block.timestamp;
         claimedMushrooms[referrals[msg.sender]] += used * 13 / 100;
-        marketMushroom += used / 5;
+        marketMushrooms += used / 5;
     }
 
     function sellMushrooms() public {
         require(initialized);
         uint256 myMushrooms = getMyMushrooms();
-        uint256 value = calculate(myMushrooms, marketMushroom, address(this).balance);
+        uint256 value = calculate(myMushrooms, marketMushrooms, address(this).balance);
         uint256 fee = getDevFee(value);
         claimedMushrooms[msg.sender] = 0;
         lastHarvest[msg.sender] = block.timestamp;
-        marketMushroom += myMushrooms;
+        marketMushrooms += myMushrooms;
         payable(owner).transfer(fee);
         payable(msg.sender).transfer(value - fee);
     }
@@ -74,8 +74,8 @@ contract Mushroom {
         return address(this).balance;
     }
 
-    function getMyMiners() public view returns (uint256) {
-        return miners[msg.sender];
+    function getMyFarmers() public view returns (uint256) {
+        return farmers[msg.sender];
     }
 
     function getMyMushrooms() public view returns (uint256) {
@@ -83,7 +83,7 @@ contract Mushroom {
     }
 
     function getMushroomsSinceLastHarvest(address _address) public view returns (uint256) {
-        return min(period, block.timestamp - lastHarvest[_address]) * miners[_address];
+        return min(period, block.timestamp - lastHarvest[_address]) * farmers[_address];
     }
 
     function min(uint256 _a, uint256 _b) private pure returns (uint256) {
